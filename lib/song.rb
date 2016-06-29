673945c766d5bf7c962744ef980b14b24bc83b5f
fidelscodes <fidelscodes@gmail.com>
2016-06-29 15:24:45 -0400

    Update with solution

---
+++ lib/song.rb
@@ -0,0 +1,57 @@
+require "pry"
+
+class Song
+  # Class Variables
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_reader :name, :artist, :genre
+
+  # #initialize, which at the same time modifies the class variables
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  # Class Methods
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    hash = {}
+    @@genres.each do |genre|
+      hash[genre] == nil ? hash[genre] = 1 : hash[genre] += 1
+    end
+    hash
+  end
+
+  def self.artist_count
+    hash = {}
+
+    @@artists.each do |artist|
+      hash[artist] == nil ? hash[artist] = 1 : hash[artist] += 1
+    end
+    hash
+  end
+
+end
+
+song_1 = Song.new("99 Problems", "Jay-Z", "rap")
+song_2 = Song.new("Ordinary People", "John Legend", "r&b")
+song_3 = Song.new("Back to Back", "Drake", "rap")
+
+# binding.pry
