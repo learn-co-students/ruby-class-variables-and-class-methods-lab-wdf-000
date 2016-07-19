21c1f92fcfbdc073798d75360662465ec661ab7a
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-19 15:40:46 -0400

    class_variables_methods

---
+++ lib/song.rb
@@ -0,0 +1,58 @@
+class Song
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_hash = {}
+  @@artist_hash = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+
+  end
+
+  def self.genre_count
+    @@genres.each do |genre|
+      if !@@genre_hash.include?(genre)
+        @@genre_hash[genre] = 1
+      else
+        @@genre_hash[genre] += 1
+      end
+
+    end
+    @@genre_hash
+
+  end
+
+  def self.artist_count
+    @@artists.each do |artist|
+      if !@@artist_hash.include?(artist)
+        @@artist_hash[artist] = 1
+      else
+        @@artist_hash[artist] += 1
+      end
+
+    end
+    @@artist_hash
+
+  end
+
+end
