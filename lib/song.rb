bdec28d78060eb16978cc467a3c8926fb408e2f8
Ekaterina Petushova <epetushova@gmail.com>
2016-10-11 21:55:26 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,20 @@
 require 'pry'
 class Song
-   @@count = 0
-   @@genres = []
-   @@artists = []
 
-   attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
     
-    def initialize(name, artist, genre)
-    	@@count += 1 
-    	@name = name
-
-    	@genre = genre
-    	@@genres << @genre
-
-    	@artist = artist
-    	@@artists << @artist
-    end
-	
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << @genre
+    @@artists << @artist
+  end
 
 	def self.count
 		@@count
@@ -29,54 +27,43 @@ class Song
 	def self.genres
 		@@genres.uniq
 	end
+  # attr_accessor added these 6 instance methods to our objects.
+  # attr_accessor :name,:artist,:genre are equivelent to the writer & reader methods
+  # def name=(name)
+  #   @name = name
+  # end
 
-
-   def name=(name)
-       @name = name
-
-   end
-
-   def name
-   	   @name 
-   end
+  # def name
+  #  	@name 
+  # end
 	
-    def artists=(artist)
-	   @artists = artist
+  # def artists=(artist)
+	 #  @artists = artist
+  # end
+
+  # def artists
+	 #  @artists
+  # end
+
+  # def genres=(genre)
+	 #  @genres = genre
+  # end
+
+  # def genres
+	 #  @genres
+  # end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+    	if artist_count[artist]
+    		artist_count[artist] += 1
+    	else
+    		artist_count[artist] = 1
+    	end
     end
-
-    def artists
-	   @artists
-
-    end
-
-    def genres=(genre)
-	   @genres = genre
-    end
-
-    def genres
-	   @genres
-    end
-
-
-    def self.artist_count
-    	artist_count = {}
-    	@@artists.each do |artist|
-    		if artist_count[artist]
-    			artist_count[artist] += 1
-    		else
-    			artist_count[artist] = 1
-    		end
-      end
-      artist_count
-    end
-
-    # def artist_count(name, number)
-    #    #artists_count = {}
-	   # # ?artists.each do |k, v|
-
-		
-	#end
-	#artist_count
+    artist_count
+  end
 
 	def self.genre_count
 		# {"pop" => 1, "rap" => 2}
@@ -88,21 +75,20 @@ class Song
 				genre_count[genre] = 1
 			end
 		end
-	genre_count
-   end
+	  genre_count
+    end
+  end
 
+  
+# {"a"=>1, "b"=>2}
 
-end
-#  {"a"=>1, "b"=>2}
-
-
+# here our Song class reveal value to us.
 # song1 = Song.new
 # song1.genre_count
-
 # Song.genre_count
 
 
  # Song.artists
  # Song.genres
  # Song.genre_count
- #Song.artist_count
+ # Song.artist_count
